0e6c7d7d6795f72b58aa10cef3bb6aac5ff605f6
sebasterreros <sebasterreros@hotmail.com>
2020-07-23 21:14:14 -0500

    Se realiza la creación de la clase spinner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Button } from "react-bootstrap";
+import Spinners from "./Components/Spinner";
 
 function App() {
   return (
     <div className="App">
       <Button variant="primary"> Probando bootstrap </Button>
+      <Spinners />
     </div>
   );
 }
---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RingLoader from "react-spinners/RingLoader";
+
+function spinners() {
+  return (
+    <div className="App">
+      <RingLoader />
+    </div>
+  );
+}
+
+export default spinners;
