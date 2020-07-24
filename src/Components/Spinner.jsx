70c74b9348f8da17d87b1da75a8897f057eb5735
sebasterreros <sebasterreros@hotmail.com>
2020-07-24 13:38:43 -0500

    Se procede a generar el spinner de carga

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import RingLoader from "react-spinners/RingLoader";
 
-function spinners() {
-  return (
-    <div className="App">
-      <RingLoader />
-    </div>
-  );
+class spinners extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
+  render() {
+    return (
+      <div className="spinner">
+        <RingLoader color="#006aff" size="80" />
+        <h3>Cargando</h3>
+      </div>
+    );
+  }
 }
 
 export default spinners;
